8e6f766eb3236354135c791040951a1eda0b9aff
Tianhao Li <ysihaoy@gmail.com>
2015-05-03 23:19:43 +1000

    week 3: http server implementation - done

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,12 @@ trait NodeScala {
    *  @param token        the cancellation token 
    *  @param response         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (token.nonCancelled && response.hasNext) {
+      exchange write response.next
+    }
+    exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +46,20 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+    Subscription(
+      listenerSubscription,
+      Future.run() { ct => async {
+        while (ct.nonCancelled) {
+          val (request, exchange) = await {
+            listener.nextRequest()
+          }
+          respond(exchange, ct, handler(request))
+       }}}
+    )
+  }
 
 }
 
