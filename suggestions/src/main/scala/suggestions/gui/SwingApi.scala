25287453b9b5d8ad95d332fe188bd1bd17f31192
Tianhao Li <ysihaoy@gmail.com>
2015-05-09 20:18:10 +1000

    week 4: solutions

--- suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.Observable
+import rx.lang.scala.{Subscription, Observable}
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,7 +51,14 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
+    def textValues: Observable[String] = {
+      Observable(observer => {
+        field subscribe {
+          case ValueChanged(tf) => observer.onNext(tf.text)
+        }
+        Subscription {}
+      })
+    }
 
   }
 
@@ -62,7 +69,14 @@ trait SwingApi {
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = ???
+    def clicks: Observable[Button] = {
+      Observable(observer => {
+        button subscribe {
+          case ButtonClicked(b) => observer.onNext(b)
+        }
+        Subscription {}
+      })
+    }
   }
 
 }
--- suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
+++ suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -37,7 +37,7 @@ trait WikipediaApi {
      *
      * E.g. `"erik", "erik meijer", "martin` should become `"erik", "erik_meijer", "martin"`
      */
-    def sanitized: Observable[String] = ???
+    def sanitized: Observable[String] = obs.map(s => s.replace(" ", "_"))
 
   }
 
@@ -48,7 +48,17 @@ trait WikipediaApi {
      *
      * E.g. `1, 2, 3, !Exception!` should become `Success(1), Success(2), Success(3), Failure(Exception), !TerminateStream!`
      */
-    def recovered: Observable[Try[T]] = ???
+    def recovered: Observable[Try[T]] = {
+      Observable { observer =>
+        obs.subscribe(
+        t => observer.onNext(Success(t)), { error: Throwable =>
+          observer.onNext(Failure(error))
+          observer.onCompleted() //On Error the observable stream has to be terminated
+        },
+        () => observer.onCompleted()
+        )
+      }
+    }
 
     /** Emits the events from the `obs` observable, until `totalSec` seconds have elapsed.
      *
@@ -56,7 +66,9 @@ trait WikipediaApi {
      *
      * Note: uses the existing combinators on observables.
      */
-    def timedOut(totalSec: Long): Observable[T] = ???
+    def timedOut(totalSec: Long): Observable[T] = {
+      obs.takeUntil(Observable.interval(totalSec second).take(1))
+    }
 
     /** Given a stream of events `obs` and a method `requestMethod` to map a request `T` into
      * a stream of responses `S`, returns a stream of all the responses wrapped into a `Try`.
@@ -83,7 +95,9 @@ trait WikipediaApi {
      *
      * Observable(Success(1), Succeess(1), Succeess(1), Succeess(2), Succeess(2), Succeess(2), Succeess(3), Succeess(3), Succeess(3))
      */
-    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = ???
+    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = {
+      obs.map(requestMethod(_).recovered.onErrorReturn(t => Failure(t))).concat
+    }
 
   }
 
